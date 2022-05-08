9dd35b20576e62f74e911688805317e7499a5c57
Ankur Paul <suryashi2013@gmail.com>
2022-05-08 16:40:49 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,21 +5,14 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get install git -y
 sudo apt install python3-pip -y
-sudo pip3 install pylint
-sudo pip3 install pipenv
-sudo pip3 install autopep8
-sudo apt-get install openjdk-11-jdk -y
+sudo apt-get install openjdk-17-jdk -y
 sudo apt install unzip
 sudo apt install g++ -y
 sudo apt install gcc -y
 sudo apt install curl -y
 sudo apt install gdb -y
-curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-sudo apt-get install -y nodejs
-sudo apt install npm -y
-sudo apt-get install iputils-ping -y
-sudo apt install nodejs -y
-sudo apt install npm -y
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+sudo apt-get install nodejs npm iputils-ping -y
 
 # Install ZSH
 sudo apt-get install -y vim zsh nano
@@ -43,21 +36,6 @@ fi
 
 # Install a plugin for ZSH auto suggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
-sudo npm i -g nodemon serve eslint typescript npkill
-
-DOTFILES_CLONE_PATH=$HOME/dotfiles
-for dotfile in "$DOTFILES_CLONE_PATH/".*; do
-  # Skip `..` and '.'
-  [[ $dotfile =~ \.{1,2}$ ]] && continue
-  # Skip Git related
-  [[ $dotfile =~ \.git$ ]] && continue
-  [[ $dotfile =~ \.gitignore$ ]] && continue
-  [[ $dotfile =~ \.gitattributes$ ]] && continue
-
-  echo "Symlinking $dotfile"
-  ln -sf "$dotfile" "$HOME"
-done
 
 cat .zshrc >> ~/.zshrc
 chsh -s $(which zsh)
