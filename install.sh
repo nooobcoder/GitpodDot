c01dd9bc0b639168a0e1e6ce4f5dc828fa1fd878
Ankur Paul <suryashi2013@gmail.com>
2022-05-10 08:38:31 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,24 +18,17 @@ sudo apt-get install nodejs npm iputils-ping -y
 sudo apt-get install -y vim zsh nano
 sudo chsh -s /bin/zsh $USER
 
+# Install and configure Oh My ZSH (if it is not already installed)
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
+
+# Install a plugin for ZSH auto suggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
 git config --global user.name "nooobcoder"
 git config --global user.email "suryashi2013@gmail.com"
 git config --global user.signingkey F4471868B502ACC037FBF1FB7C2D646CF53E03BD
 git config --global commit.gpgsign true  
 
-# Install and configure Oh My ZSH (if it is not already installed)
-if [ -d "~/.oh-my-zsh" ] 
-then
-    echo "oh-my-zsh is already installed" 
-else
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-    # Append zshrc stuff to end of file
-    touch ~/.zshrc # not sure if this will always exist at this point :/
-    cat .zshrc >> ~/.zshrc
-fi
-
-# Install a plugin for ZSH auto suggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
-cat .zshrc >> ~/.zshrc
+cat ./.zshrc >> ~/.zshrc
 chsh -s $(which zsh)
