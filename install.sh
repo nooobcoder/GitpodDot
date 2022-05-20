46326329cb81bbbb77e5618be4278088b9c49b35
Ankur Paul <suryashi2013@gmail.com>
2022-05-20 11:04:50 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,16 +3,6 @@
 
 sudo apt-get update -y
 sudo apt-get upgrade -y
-sudo apt-get install git -y
-sudo apt install python3-pip -y
-sudo apt-get install openjdk-17-jdk -y
-sudo apt install unzip
-sudo apt install g++ -y
-sudo apt install gcc -y
-sudo apt install curl -y
-sudo apt install gdb -y
-curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
-sudo apt-get install nodejs npm iputils-ping -y
 
 # Install ZSH
 sudo apt-get install -y vim zsh nano
